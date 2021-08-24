3ed2e234614788238f1f29323251933bf3ecd7a5
David Marquinez Burgos <d.marquinez@alumnos.upm.es>
2021-08-24 23:00:55 +0200

    Added imgs, presentation and documentation

--- pi/server.R
+++ pi/server.R
@@ -14,6 +14,20 @@ shinyServer(function(input, output) {
                                    "Red", "Blue"))
         df
     })
+    output$plot2 <- renderPlot({
+        plot(0,0, asp=1,
+             xlim = c(-1, 1), ylim = c(-1, 1),
+             xlab = "", ylab = "",
+             yaxt='n', xaxt='n', ann=FALSE, bty='n')
+        draw.circle(0, 0, 1, border = "red")
+        mult_seg <- data.frame(x0=c(-1,-1,-1,1,0,0),
+                               y0=c(-1,-1,1,-1,0,0),
+                               x1=c(1,-1,1,1,1,0),
+                               y1=c(-1,1,1,1,0,1))
+        segments(x0 = mult_seg$x0,y0 = mult_seg$y0,
+                 x1 = mult_seg$x1,y1 = mult_seg$y1)
+        text(0.5, -0.1, "r", cex = 3)
+    })
     output$plot <- renderPlot({
         df <- createdf()
         plot(df$X, df$Y, asp = 1, 
@@ -23,6 +37,14 @@ shinyServer(function(input, output) {
         calc <- 4*sum(df$Color=="Red")/input$num
         output$ppi <- renderText({
             paste("Calculated value for pi is: ",calc)
+                  })
+        output$ppi2 <- renderText({
+            paste("Absolute error is: "
+                  ,abs(pi-calc))
+                  })
+        output$ppi3 <- renderText({
+            paste("Percentage error is: ",
+                  round(100*abs(pi-calc)/calc,2),"%")
         })
     })
 
--- pi/ui.R
+++ pi/ui.R
@@ -4,7 +4,7 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel(
             numericInput("num",
-                        "Number of points:",
+                        "Choose the number of points to generate:",
                         min = 1,
                         max = 100000,
                         value = 1000),
@@ -15,9 +15,29 @@ shinyUI(fluidPage(
         ),
         
         mainPanel(
-            h1("Pi"),
+            h1("Given a function that generates random numbers between 0 and 1 calculate pi"),
+            hr(),
             textOutput("ppi"),
+            textOutput("ppi2"),
+            textOutput("ppi3"),
             plotOutput("plot", height = "400px", width = "400px"),
+            h1("The maths behind"),
+            hr(),
+            p("We need to refresh some basic formulas of geometrics before moving on"),
+            p("Area of Square = side^2"),
+            p("Area of Circle = π*r^2"),
+            plotOutput("plot2",height = "400px", width = "400px"),
+            p("If we start generating points the number of points inside the circle
+              divided by the total number of points is going to be equal to
+              one-quarter of the area of the circle divided by the area of the square:"),
+            br(),
+            img(src="img1.gif"),
+            br(),
+            br(),
+            p("If we simplify:"),
+            img(src="img2.gif"),
+            br(),
+            hr(),
             HTML("<br><p>Small app inspired by <a href='https://youtu.be/pvimAM_SLic' target='_blank'> this video</a>
                  of <a href='https://twitter.com/jomaoppa'>Joma Tech</a> and the lectures of <a href='http://www.bcaffo.com/' target='_blank'> 
                  Brian Caffo</a> in the <a href='https://www.coursera.org/learn/data-products'
